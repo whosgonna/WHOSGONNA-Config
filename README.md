733d44e228305e36299b1237fe723573b53d071a
Ben Kaufman <ben.kaufman@altigen.com>
2019-03-18 13:09:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+AltiGen::Config - It's new $module
+
+# SYNOPSIS
+
+    use AltiGen::Config;
+
+# DESCRIPTION
+
+AltiGen::Config is ...
+
+# LICENSE
+
+Copyright (C) Ben Kaufman.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Ben Kaufman <ben.kaufman@altigen.com>
---
+++ lib/AltiGen/Config.pm
@@ -0,0 +1,39 @@
+package AltiGen::Config;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+AltiGen::Config - It's new $module
+
+=head1 SYNOPSIS
+
+    use AltiGen::Config;
+
+=head1 DESCRIPTION
+
+AltiGen::Config is ...
+
+=head1 LICENSE
+
+Copyright (C) Ben Kaufman.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Ben Kaufman E<lt>ben.kaufman@altigen.comE<gt>
+
+=cut
+
